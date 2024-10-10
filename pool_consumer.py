c91ce090292927c80f2d9ecff2af877f4de8f355
reyel <felipe.reyel@abstra.app>
2024-10-10 16:22:15 -0300

    pool consumer

---
+++ pool_consumer.py
@@ -0,0 +1,24 @@
+import concurrent.futures
+import functools
+
+from pika import BlockingConnection
+from pika.adapters.blocking_connection import BlockingChannel
+
+from base_consumer import run
+from work import ack, work
+
+
+def done_callback(connection, channel, method_frame, result):
+    connection.add_callback_threadsafe(lambda: ack(channel, method_frame))
+
+
+def pool_consumer(connection: BlockingConnection, channel: BlockingChannel, queue: str):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for method_frame, header_frame, body in channel.consume(queue=queue):
+            result = executor.submit(work, body, method_frame)
+            cb = functools.partial(done_callback, connection, channel, method_frame)
+            result.add_done_callback(cb)
+
+
+if __name__ == "__main__":
+    run(pool_consumer)
