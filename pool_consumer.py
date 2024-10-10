db0aa01710cb8cb4d8de88c51947b32fc578ea67
reyel <felipe.reyel@abstra.app>
2024-10-10 16:44:30 -0300

    max tasks

--- pool_consumer.py
+++ pool_consumer.py
@@ -6,6 +6,7 @@ from pika.adapters.blocking_connection import BlockingChannel
 
 from base_consumer import run
 from work import ack, work
+from configs import PREFETCH_COUNT
 
 
 def done_callback(connection, channel, method_frame, result):
@@ -13,10 +14,12 @@ def done_callback(connection, channel, method_frame, result):
 
 
 def pool_consumer(connection: BlockingConnection, channel: BlockingChannel, queue: str):
-    with concurrent.futures.ProcessPoolExecutor(max_tasks_per_child=5) as executor:
+    with concurrent.futures.ProcessPoolExecutor(
+        max_workers=PREFETCH_COUNT, max_tasks_per_child=5
+    ) as executor:
         for method_frame, header_frame, body in channel.consume(queue=queue):
-            result = executor.submit(work, body, method_frame)
             cb = functools.partial(done_callback, connection, channel, method_frame)
+            result = executor.submit(work, body, method_frame)
             result.add_done_callback(cb)
 
 
